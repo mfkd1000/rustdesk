12e276e5809e5e7b7b9048cb8226710c30756ef7
mfkd1000 <73184372+mfkd1000@users.noreply.github.com>
2024-08-24 13:05:19 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustipv6.mfkd1000.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "+syWi5XMjM37Ia6DB9CzUMEuR9aZmyFRiTe4codpaTY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
