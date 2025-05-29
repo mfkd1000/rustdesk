6912d4a6071c2f9a9ce71d79029311378973d23b
mfkd1000 <73184372+mfkd1000@users.noreply.github.com>
2025-05-29 08:45:16 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rustipv6.mfkd1000.top:7000".to_owned()
 }
 
 #[inline]
